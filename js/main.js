b27e596aea40189d84a07b4a01f665550abf629e
sabrina palm <sabrina_palm@hotmail.com>
2018-03-06 17:37:42 +0100

    disabled scrolldown effect on mobile phone

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-$(function(){  // $(document).ready shorthand
-  $('#container').fadeIn('slow');
+    
+    
+    
+    
+ if ( $(window).width() <= 600 ) {
+     
+     $( ".contact" ).removeClass( "hideme" );
+     $( ".container" ).removeClass( "hideme" );
+   
+ } else {
+   $(function(){  // $(document).ready shorthand
+  $('.container').fadeIn('fast');
 });
 
 $(document).ready(function() {
@@ -18,7 +27,7 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},1500);
+                $(this).animate({'opacity':'1'},1200);
                     
             }
             
@@ -27,6 +36,27 @@ $(document).ready(function() {
     });
     
 });
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
