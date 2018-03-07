2643ff251046cd1b0c363e3662ff4cd85a98774e
sabrina palm <sabrina_palm@hotmail.com>
2018-03-07 21:37:25 +0100

    update+, not done

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    
-    
-    
- if ( $(window).width() <= 600 ) {
-     
-     $( ".contact" ).removeClass( "hideme" );
-     $( ".container" ).removeClass( "hideme" );
-   
- } else {
-   $(function(){  // $(document).ready shorthand
-  $('.container').fadeIn('fast');
+	if ($(window).width() <= 600) {
+		$(".contact").removeClass("hideme");
+		$(".container").removeClass("hideme");
+	} else {
+		$(function () { // $(document).ready shorthand
+			$('.container').fadeIn('fast');
+		});
+		$(document).ready(function () {
+			/* Every time the window is scrolled ... */
+			$(window).scroll(function () {
+				/* Check the location of each desired element */
+				$('.hideme').each(function (i) {
+					var bottom_of_object = $(this).position().top + $(this).outerHeight();
+					var bottom_of_window = $(window).scrollTop() + $(window).height();
+					/* If the object is completely visible in the window, fade it it */
+					if (bottom_of_window > bottom_of_object) {
+						$(this).animate({
+							'opacity': '1'
+						}, 1200);
+					}
+				});
+			});
+		});
+	}
+
+
+    $("#clickme").click(function(){
+        
+        $("#about-me").html("😎");
+        $("#education-me").html("🏫");
+        $("#portfolio-me").html("📓");
+        $("#contact-me").html("📩");
+        $("#clickme").html("💚");
+        
+        $("#about-me").css("fontSize", "1em");
+        $("#education-me").css("fontSize", "1em");
+        $("#portfolio-me").css("font-size", "1em");
+        $("#contact-me").css("font-size", "1em");
+
 });
 
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1200);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
-     
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
