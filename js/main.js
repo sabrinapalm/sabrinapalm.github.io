ae97f4a8093afb86022661dfd2ddccf82c4ac9d4
sabrina palm <sabrina_palm@hotmail.com>
2018-03-07 22:31:53 +0100

    +

--- js/main.js
+++ js/main.js
@@ -2,19 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
 	if ($(window).width() <= 600) {
 		$(".contact").removeClass("hideme");
 		$(".container").removeClass("hideme");
+        
 	} else {
-		$(function () { // $(document).ready shorthand
+        
+		$(function () { 
+            // $(document).ready shorthand
 			$('.container').fadeIn('fast');
 		});
 		$(document).ready(function () {
+            
 			/* Every time the window is scrolled ... */
 			$(window).scroll(function () {
+                
 				/* Check the location of each desired element */
 				$('.hideme').each(function (i) {
+                    
 					var bottom_of_object = $(this).position().top + $(this).outerHeight();
 					var bottom_of_window = $(window).scrollTop() + $(window).height();
+                    
 					/* If the object is completely visible in the window, fade it it */
 					if (bottom_of_window > bottom_of_object) {
+                        
 						$(this).animate({
 							'opacity': '1'
 						}, 1200);
@@ -24,20 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	}
     
+    
+    //Change menu to emojis <3
     $('#clickme').click(function() {
-      var clicks = $(this).data('clicks');
+      let clicks = $(this).data('clicks');
       if (clicks) {
           
           $("#about-me").html("ABOUT ME");
           $("#education-me").html("EDUCATION");
           $("#portfolio-me").html("PORTFOLIO");
           $("#contact-me").html("CONTACT");
+          
           $("#clickme").html("💛");
         
-          $("#about-me").css("fontSize", "0.7em");
-          $("#education-me").css("fontSize", "0.7em");
-          $("#portfolio-me").css("font-size", "0.7em");
-          $("#contact-me").css("font-size", "0.7em");
+          $("#about-me, #education-me, #portfolio-me, #contact-me").css("fontSize", "0.7em");
           
       } else {
           
@@ -45,12 +53,10 @@ document.addEventListener('DOMContentLoaded', () => {
           $("#education-me").html("👩‍🎓");
           $("#portfolio-me").html("💼");
           $("#contact-me").html("📩");
+          
           $("#clickme").html("❤️");
           
-          $("#about-me").css("fontSize", "1em");
-          $("#education-me").css("fontSize", "1em");
-          $("#portfolio-me").css("font-size", "1em");
-          $("#contact-me").css("font-size", "1em");
+          $("#about-me, #education-me, #portfolio-me, #contact-me").css("fontSize", "1em");
           
       }
         
