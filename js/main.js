75ed4f35b068f5bd4360b4b03a54c2f5e95ff419
sabrina palm <sabrina_palm@hotmail.com>
2019-03-31 22:55:15 +0200

    added lightgreen as accentcolor

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@
 
 	function getRandomColorOnLoad() {
 	 	var root = document.documentElement;
-		var colors = ["#41aaa8", "#c54fa7", "#f69314", "#6b76ff"];
+		var colors = ["#41aaa8", "#c54fa7", "#f69314", "#6b76ff", "#a6cb12"];
 
 		function getColor() {
 		   return colors[
