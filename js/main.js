098e71bb717dbb091ade9cc1dd01f228f3f52f86
sabrina palm <sabrina_palm@hotmail.com>
2019-04-08 19:02:48 +0200

    changed profile picture, added new bg video, added new color as accent

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@
 
 	function getRandomColorOnLoad() {
 	 	var root = document.documentElement;
-		var colors = ["#41aaa8", "#c54fa7", "#f69314", "#6b76ff", "#a6cb12", "#df0e62"];
+		var colors = ["#41aaa8", "#D37A90", "#f69314", "#6b76ff", "#a6cb12", "#df0e62"];
 
 		function getColor() {
 		   return colors[
